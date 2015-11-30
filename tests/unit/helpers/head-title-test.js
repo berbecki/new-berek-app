90b4f2d45fc501aa80971e3028b4bfb68357939c
berbecki <berbecki@gmail.com>
2015-11-30 23:00:18 +0100

    Add title helper

---
+++ tests/unit/helpers/head-title-test.js
@@ -0,0 +1,10 @@
+import { headTitle } from '../../../helpers/head-title';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | head title');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = headTitle(42);
+  assert.ok(result);
+});
